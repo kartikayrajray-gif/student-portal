c28740b36d512ef30aef42d110f9fe9b5428f061
kartikayrajray-gif <kartikayrajray@gmail.com>
2026-06-15 23:27:23 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,59 @@
+package com.example.studentportal;
+
+public class Student {
+
+    private String rollNo;
+    private String name;
+    private String branch;
+    private String semester;
+    private String email;
+    private String mobile;
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
